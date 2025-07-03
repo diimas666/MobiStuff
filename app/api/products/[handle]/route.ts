24db9a5855174b5b3b5c37c41f4e1d0ef2863f6c
dima tikhtey <uu36548@gmail.com>
2025-07-03 17:56:24 +0300

    идет переход на страницу товара все показывает

--- app/api/products/[handle]/route.ts
+++ app/api/products/[handle]/route.ts
@@ -18,15 +18,13 @@ export async function GET(
   } catch (error: any) {
     return NextResponse.json({ error: error.message }, { status: 500 });
   }
-// }
+}
+
 // Это API-роут, который:
 
 // ищет один товар по его handle (уникальный slug товара),
 
 // возвращает JSON-объект товара,
-
-
-
 
 // const res = await fetch(`/api/products/33wdsd`);
 // const product = await res.json();
