80e4d4ca42cb3466b1784e08512e1e911f8fdf10
dima tikhtey <uu36548@gmail.com>
2025-07-28 11:22:54 +0300

    убрад масштаб в инпута фильтрации на моб

--- components/FilterBar.tsx
+++ components/FilterBar.tsx
@@ -48,7 +48,7 @@ export default function FilterBar() {
             name="minPrice"
             defaultValue={minPrice}
             placeholder="0"
-            className="border border-gray-300 rounded-md px-3 py-2 text-sm w-[120px] shadow-sm focus:outline-none focus:ring-2 focus:ring-black"
+            className="text-[16px] border border-gray-300 rounded-md px-3 py-2 text-sm w-[120px] shadow-sm focus:outline-none focus:ring-2 focus:ring-black"
           />
         </div>
 
@@ -60,7 +60,7 @@ export default function FilterBar() {
             name="maxPrice"
             defaultValue={maxPrice}
             placeholder="1000"
-            className="border border-gray-300 rounded-md px-3 py-2 text-sm w-[120px] shadow-sm focus:outline-none focus:ring-2 focus:ring-black"
+            className="text-[16px] border border-gray-300 rounded-md px-3 py-2 text-sm w-[120px] shadow-sm focus:outline-none focus:ring-2 focus:ring-black"
           />
         </div>
 
